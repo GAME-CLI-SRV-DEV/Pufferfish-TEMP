95af9d13634398d54b7484e4d4b991afe0f75b29
Krakenied <Krakenied1@gmail.com>
2024-05-09 19:11:13 +0200

    Update to 1.20.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
 
 plugins {
     java
-    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.15"
+    id("io.github.goooler.shadow") version "8.1.7" apply false
+    id("io.papermc.paperweight.patcher") version "1.6.3"
 }
 
 repositories {
@@ -23,12 +23,12 @@ subprojects {
     apply(plugin = "java")
 
     java {
-        toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
+        toolchain { languageVersion.set(JavaLanguageVersion.of(21)) }
     }
 
     tasks.withType<JavaCompile>().configureEach {
         options.encoding = "UTF-8"
-        options.release.set(17)
+        options.release.set(21)
     }
 
     repositories {
